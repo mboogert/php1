7adf7cb85b2752fbfb6e16728ac2908f3654a060
Marcel Boogert <marcel@mtdb.nl>
2016-07-15 20:45:31 +0200

    update

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
+
   print "<body style='background-color:red'>";
   header("Refresh:1");
-  print $_SERVER['SERVER_NAME'];
-  print "</br>";
-  print $_SERVER['HTTP_HOST'];
-  print "</br>";
-  print $_SERVER['SERVER_ADDR'];
+  
+  print "SERVER_NAME = " . $_SERVER['SERVER_NAME'] . "</br>";
+  print "HTTP_HOST   = " . $_SERVER['HTTP_HOST'] . "</br>"
+  print "SERVER_ADDR = " . $_SERVER['SERVER_ADDR'] . "</br></br>"
+  
+  $time = microtime();
+  $time = explode(' ', $time);
+  $time = $time[1] + $time[0];
+  $finish = $time;
+  $total_time = round(($finish - $start), 4);
+  echo 'Page generated in '.$total_time.' seconds.';
+
 ?>
