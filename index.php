612942db877b8b442644b5f6eabe08ef790fad46
Marcel Boogert <marcel@mtdb.nl>
2016-07-15 20:39:14 +0200

    changed color to red

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
+  print "<body style='background-color:red'>";
   header("Refresh:1");
-  echo "<body style='background-color:pink'>";
-  echo $_SERVER['SERVER_NAME'];
-  echo "\n";
-  echo $_SERVER['HTTP_HOST'];
-  echo "\n";
-  echo $_SERVER['SERVER_ADDR'];
+  print $_SERVER['SERVER_NAME'];
+  print "\n";
+  print $_SERVER['HTTP_HOST'];
+  print "\n";
+  print $_SERVER['SERVER_ADDR'];
 ?>
